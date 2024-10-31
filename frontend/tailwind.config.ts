6c06c06b64c6d21271bd2cb965210e8be5aeef51
LucasGdBS <lgbs@cesar.school>
2024-10-31 14:38:20 -0300

    adiciona shadcn

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,41 +1,84 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "#00696a",
-        "primary-container": "#00a6a7",
-        "on-primary": "#ffffff",
-        "on-primary-container": "#002020",
-        "secondary": "#4a6363",
-        "secondary-container": "#cce8e7",
-        "on-secondary": "#ffffff",
-        "on-secondary-container": "#041f20",
-        "error": "ba1a1a",
-        "error-container": "#ffdad6",
-        "on-error": "#ffffff",
-        "on-error-container": "#410002",
-        "surface": "#f2f2f2",
-        "on-surface": "#161d1d",
-        "sur-container-low": "#eff5f4",
-        "sur-container": "#e9efee",
-        "outline": "6f7979",
-      },
-      fontFamily: {
-        sans: "var(--font-geist-sans)",
-        mono: "var(--font-geist-mono)",
-        montserrat: "var(--font-montserrat)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-container': '#00a6a7',
+  			'on-primary': '#ffffff',
+  			'on-primary-container': '#002020',
+  			'secondary-color': '#4a6363',
+  			'secondary-container': '#cce8e7',
+  			'on-secondary': '#ffffff',
+  			'on-secondary-container': '#041f20',
+  			'error': 'ba1a1a',
+  			'error-container': '#ffdad6',
+  			'on-error': '#ffffff',
+  			'on-error-container': '#410002',
+  			'surface': '#f2f2f2',
+  			'on-surface': '#161d1d',
+  			'sur-container-low': '#eff5f4',
+  			'sur-container': '#e9efee',
+  			'outline': '6f7979',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: 'var(--font-geist-sans)',
+  			mono: 'var(--font-geist-mono)',
+  			montserrat: 'var(--font-montserrat)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
