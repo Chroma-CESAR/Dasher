af91334c3e3819c5d2a6f331c124b8166f232f9e
LucasGdBS <lgbs@cesar.school>
2024-11-21 22:05:37 -0300

    resolvendo bug do docker compose e retirando variaveis de ambiente

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+// const baseURL = process.env.NEXT_PUBLIC_API_URL;
+//Não consegui passar a variável de ambiente pelo docker-compose
+
+const baseURL = "http://localhost:8001";
 
 const axiosInstance = axios.create({
   baseURL,
