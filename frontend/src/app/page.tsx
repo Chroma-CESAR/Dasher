4e772cae90f3348d293abb8d85e0f1015e08ea4f
Caiobadv <caiobarretodv@hotmail.com>
2024-11-25 16:45:46 -0300

    dashboard no FRONT WOW

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,13 @@ export default function Home() {
     <div>
       <Header classname="pr-6 pl-5" />
       <main className="flex flex-col h-screen justify-center items-center">
-        Dashboard
+        <iframe
+          src="http://localhost:3002/d/be51fax9t2z9cd/variacao-do-saldo-ao-longo-do-tempo?orgId=1&from=2024-01-02T00:00:00.000Z&to=2024-08-12T00:00:00.000Z&timezone=browser"
+          width="100%"
+          height="800"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
       </main>
     </div>
   );
