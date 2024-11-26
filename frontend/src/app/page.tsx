ab818880d5aeb1664bd77ff3d73b9d564ac4053e
Gabriel <g.chaves0110@gmail.com>
2024-11-26 16:32:26 -0300

    :lipstick: feat: Ajeitando formatação das tabelas do dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,13 +5,27 @@ export default function Home() {
   return (
     <div>
       <Header classname="pr-6 pl-5" />
-      <main className="flex flex-col h-screen justify-center items-center">
-        <iframe
-          src="http://localhost:3002/d/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=2024-01-01T00:00:00.000Z&to=2024-08-12T00:00:00.000Z&timezone=browser"
-          width="100%"
-          height="800"
-        ></iframe>
+      <main className="flex flex-col h-screen">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 p-6 w-full">
+          <iframe
+            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=4&__feature.dashboardSceneSolo"
+            className="w-full h-64 border rounded-lg shadow-lg"
+          ></iframe>
+          <iframe
+            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=1&__feature.dashboardSceneSolo"
+            className="w-full h-64 border rounded-lg shadow-lg"
+          ></iframe>
+          <iframe
+            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=3&__feature.dashboardSceneSolo"
+            className="w-full h-64 border rounded-lg shadow-lg"
+          ></iframe>
+          <iframe
+            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=2&__feature.dashboardSceneSolo"
+            className="w-full h-64 border rounded-lg shadow-lg"
+          ></iframe>
+        </div>
       </main>
     </div>
   );
 }
+
