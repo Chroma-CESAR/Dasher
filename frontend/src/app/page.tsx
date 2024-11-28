93e03324a21ff19ae3684aa4fa167e13e1e5fe4e
LucasGdBS <lgbs@cesar.school>
2024-11-27 21:53:47 -0300

    Ajustando graficos importados do grafana

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,25 +2,30 @@
 import Header from "@/components/Header";
 
 export default function Home() {
+  const urlGrafana = process.env.NEXT_PUBLIC_GRAFANA_URL;
+  const graph1 = process.env.NEXT_PUBLIC_GRAPH1
+  const graph2 = process.env.NEXT_PUBLIC_GRAPH2
+  const graph3 = process.env.NEXT_PUBLIC_GRAPH3
+  const graph4 = process.env.NEXT_PUBLIC_GRAPH4
   return (
     <div>
       <Header classname="pr-6 pl-5" />
       <main className="flex flex-col h-screen">
         <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 p-6 w-full">
           <iframe
-            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=4&__feature.dashboardSceneSolo"
+            src={`${urlGrafana}${graph1}`}
             className="w-full h-64 border rounded-lg shadow-lg"
           ></iframe>
           <iframe
-            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=1&__feature.dashboardSceneSolo"
+            src={`${urlGrafana}${graph2}`}
             className="w-full h-64 border rounded-lg shadow-lg"
           ></iframe>
           <iframe
-            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=3&__feature.dashboardSceneSolo"
+            src={`${urlGrafana}${graph3}`}
             className="w-full h-64 border rounded-lg shadow-lg"
           ></iframe>
           <iframe
-            src="http://localhost:3002/d-solo/ae4xrbe6nhwxsc/d6d770d?orgId=1&from=1704067200000&to=1723420800000&timezone=browser&theme=light&panelId=2&__feature.dashboardSceneSolo"
+            src={`${urlGrafana}${graph4}`}
             className="w-full h-64 border rounded-lg shadow-lg"
           ></iframe>
         </div>
